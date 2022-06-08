82f9671aeaa3c47bf3cf9be3f69f002b84c3d573
Giacomo Castiglioni <giacastiglioni@gmail.com>
2022-06-08 13:18:10 +0200

    eigen openmp dep and mkl backend options (#337)

--- cmake.d/BundleEigen.cmake
+++ cmake.d/BundleEigen.cmake
@@ -68,6 +68,15 @@ if(NOT ${QUICC_EIGEN_ENABLE_OPENMP})
     target_compile_options(${_aliased} INTERFACE "-DEIGEN_DONT_PARALLELIZE")
     get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
     message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
+else()
+    get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
+    message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
+    # retrieve original target, we cannot set properties on aliased targets
+    get_target_property(_aliased Eigen3::Eigen ALIASED_TARGET)
+    target_compile_options(${_aliased} INTERFACE "-fopenmp")
+    target_link_options(${_aliased} INTERFACE "-fopenmp")
+    get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
+    message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
 endif()
 
 # Enable/disable vectorization
@@ -96,6 +105,25 @@ if(NOT ${QUICC_EIGEN_ENABLE_CUDA})
     target_compile_options(${_aliased} INTERFACE "-DEIGEN_NO_CUDA")
     get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
     message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
+endif()
+
+# Enable/disable MKL
+option(QUICC_EIGEN_ENABLE_MKL "Enable/disable Eigen MKL backend" OFF)
+mark_as_advanced(QUICC_EIGEN_ENABLE_MKL)
+
+if(${QUICC_EIGEN_ENABLE_MKL})
+    get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
+    message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
+    # retrieve original target, we cannot set properties on aliased targets
+    get_target_property(_aliased Eigen3::Eigen ALIASED_TARGET)
+    target_compile_options(${_aliased} INTERFACE "-DEIGEN_USE_MKL_ALL")
+    get_target_property(Eigen_INTERFACE_COMPILE_OPTIONS Eigen3::Eigen INTERFACE_COMPILE_OPTIONS)
+    message(DEBUG "Eigen_INTERFACE_COMPILE_OPTIONS: ${Eigen_INTERFACE_COMPILE_OPTIONS}")
+
+    # need MKL
+    set(BLA_VENDOR Intel10_64lp)
+    find_package(BLAS REQUIRED)
+    target_link_libraries(${_aliased} INTERFACE BLAS::BLAS)
 endif()
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
