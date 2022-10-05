660db8b3d8021399de357c349d098ef9df910c2f
Philippe Marti <philippe.marti@erdw.ethz.ch>
2022-10-05 17:53:48 +0200

    Full 3D transform tests (#421)
    Full 3D transform tests

---
+++ Components/Framework/TestSuite/include/QuICC/TestSuite/Framework/TransformCoordinator/TestArgs.hpp
@@ -0,0 +1,78 @@
+/**
+ * @file TestArgs.hpp
+ * @brief Command line arguments for tests
+ */
+
+#ifndef QUICC_TESTSUITE_FRAMEWORK_TCOORD_TESTARGS_HPP
+#define QUICC_TESTSUITE_FRAMEWORK_TCOORD_TESTARGS_HPP
+
+// Configuration includes
+//
+
+// System includes
+//
+#include <vector>
+#include <string>
+
+// Project includes
+//
+
+namespace QuICC {
+
+namespace TestSuite {
+
+namespace Framework {
+
+namespace TCoord {
+
+   struct TestArgs
+   {
+      /// Use default test setup
+      bool useDefault;
+
+      /// Write output data to file
+      bool dumpData;
+
+      /// Only time execution, don't check data
+      bool timeOnly;
+
+      /// Max ulp
+      unsigned int ulp;
+
+      /// Dimension 1D
+      unsigned int dim1D;
+
+      /// Dimension 1D
+      unsigned int dim2D;
+
+      /// Dimension 1D
+      unsigned int dim3D;
+
+      /// Comm splitting algorithm
+      std::string algorithm;
+
+      /// Comm grouping algorithm
+      std::string grouper;
+
+      /// ID of the tests
+      std::vector<int> params;
+
+      /**
+       * @brief Constructor
+       */
+      TestArgs();
+
+      /**
+       * @brief Destructor
+       */
+      ~TestArgs() = default;
+   };
+
+   TestArgs& args();
+
+}
+}
+}
+}
+
+#endif //QUICC_TESTSUITE_FRAMEWORK_TCOORD_TESTARGS_HPP
