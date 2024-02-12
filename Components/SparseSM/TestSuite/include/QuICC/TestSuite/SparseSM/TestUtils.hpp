41a6b33974e2c73d8dea69bde4bcd3b23b49b472
Philippe Marti (garulfo) <philippe.marti@colorado.edu>
2024-02-12 13:16:26 +0100

    Fix MP

--- Components/SparseSM/TestSuite/include/QuICC/TestSuite/SparseSM/TestUtils.hpp
+++ Components/SparseSM/TestSuite/include/QuICC/TestSuite/SparseSM/TestUtils.hpp
@@ -22,7 +22,7 @@ namespace SparseSM {
    /**
     * @Brief Compute ULP
     */
-   std::pair<bool, MHDFloat> computeUlp(const ::QuICC::MHDFloat data, const ::QuICC::MHDFloat ref, const ::QuICC::MHDFloat refMod, const ::QuICC::MHDFloat maxUlp, const ::QuICC::MHDFloat epsilon);
+   std::pair<bool, MHDFloat> computeUlp(const MHDFloat data, const MHDFloat ref, const MHDFloat refMod, const MHDFloat maxUlp, const MHDFloat epsilon);
 
    /**
     * @brief Check stencils by computing boundary values
