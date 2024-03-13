c84fdc6a08d4d8a0c13afe79f7ed1c0f9ce88e0c
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-03-13 14:18:53 +0100

    read and dump

--- Components/Graph/TestSuite/GraphTests.cpp
+++ Components/Graph/TestSuite/GraphTests.cpp
@@ -4,15 +4,17 @@
 #include <catch2/catch.hpp>
 #include <memory>
 
-#include <Quiccir-c/Dialects.h>
-#include <mlir-c/IR.h>
-#include <mlir-c/RegisterEverything.h>
+#include <Quiccir/IR/QuiccirDialect.h>
+#include <Quiccir/Transforms/QuiccirPasses.h>
+
+#include <mlir/InitAllDialects.h>
+#include <mlir/Dialect/Func/Extensions/AllExtensions.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/Parser/Parser.h>
 
 // #include "Memory/Cpu\NewDelete.hpp"
 // #include "Memory/Cuda/Malloc.hpp"
 // #include "Memory/Memory.hpp"
-// #include "Qir/Qir.hpp"
-// #include "QirBuilder/Builder.hpp"
 // #include "ViewOps/Pointwise/Cpu/Pointwise.hpp"
 // #include "Visitor/Transform/Visitor.hpp"
 // #include "ViewOps/ViewMemoryUtils.hpp"
@@ -31,46 +33,39 @@ int main()
    return returnCode;
 }
 
-/// @brief \todo only register what's needed
-/// @param ctx
-static void registerAllUpstreamDialects(MlirContext ctx) {
-  MlirDialectRegistry registry = mlirDialectRegistryCreate();
-  mlirRegisterAllDialects(registry);
-  mlirContextAppendDialectRegistry(ctx, registry);
-  mlirDialectRegistryDestroy(registry);
-}
-
 TEST_CASE("Simple Tree", "[SimpleTree]")
 {
-  MlirContext ctx = mlirContextCreate();
-  registerAllUpstreamDialects(ctx);
+  mlir::DialectRegistry registry;
+  mlir::func::registerAllExtensions(registry);
+  // Add the following to include *all* MLIR Core dialects, or selectively
+  // include what you need like above. You only need to register dialects that
+  // will be *parsed* by the tool, not the one generated
+  registerAllDialects(registry);
 
-  mlirDialectHandleRegisterDialect(mlirGetDialectHandle__quiccir__(), ctx);
+  mlir::MLIRContext ctx(registry);
+  // Load our Dialect in this MLIR Context.
+  ctx.loadDialect<mlir::quiccir::QuiccirDialect>();
 
-  MlirModule module = mlirModuleCreateParse(
-      ctx, mlirStringRefCreateFromCString(
-        "!type_umod = !quiccir.view<1x2x3xf64, \"layoutUmod\">\n"
-        "!type_uval = !quiccir.view<1x3x3xf64, \"layoutUval\">\n"
-        "!type_tumod = tensor<1x2x3xf64, \"layoutUmod\">\n"
-        "!type_tuval = tensor<1x3x3xf64, \"layoutUval\">\n"
-        "func.func @entry(%thisArr: !llvm.ptr<array<2 x ptr>> {llvm.noalias}, %uout: !type_umod, %umod: !type_umod) {\n"
-        "  %tumod = builtin.unrealized_conversion_cast %umod : !type_umod to !type_tumod\n"
-        "  %tuval = quiccir.jw.prj %tumod : !type_tumod -> !type_tuval attributes{implptr = 0 :i64}\n"
-        "  %ret = quiccir.jw.int %tuval : !type_tuval -> !type_tumod attributes{implptr = 1 :i64}\n"
-        "  quiccir.materialize %ret in %uout : (!type_tumod, !type_umod)\n"
-        "  return\n"
-        "}\n"));
-  if (mlirModuleIsNull(module)) {
-    printf("ERROR: Could not parse.\n");
-    mlirContextDestroy(ctx);
-    CHECK(false);
-  }
-  else {
-    MlirOperation moduleOp = mlirModuleGetOperation(module);
-    mlirOperationDump(moduleOp);
+  std::string modStr =
+    "!type_umod = !quiccir.view<1x2x3xf64, \"layoutUmod\">\n"
+    "!type_uval = !quiccir.view<1x3x3xf64, \"layoutUval\">\n"
+    "!type_tumod = tensor<1x2x3xf64, \"layoutUmod\">\n"
+    "!type_tuval = tensor<1x3x3xf64, \"layoutUval\">\n"
+    "func.func @entry(%thisArr: !llvm.ptr<array<2 x ptr>> {llvm.noalias}, %uout: !type_umod, %umod: !type_umod) {\n"
+    "  %tumod = builtin.unrealized_conversion_cast %umod : !type_umod to !type_tumod\n"
+    "  %tuval = quiccir.jw.prj %tumod : !type_tumod -> !type_tuval attributes{implptr = 0 :i64}\n"
+    "  %ret = quiccir.jw.int %tuval : !type_tuval -> !type_tumod attributes{implptr = 1 :i64}\n"
+    "  quiccir.materialize %ret in %uout : (!type_tumod, !type_umod)\n"
+    "  return\n"
+    "}\n";
 
-    mlirModuleDestroy(module);
-    mlirContextDestroy(ctx);
-  }
+  // Load
+  mlir::OwningOpRef<mlir::ModuleOp> module;
+  module = mlir::parseSourceString<mlir::ModuleOp>(modStr, &ctx);
+
+  // Echo
+  module->dump();
+
+
 
 }
