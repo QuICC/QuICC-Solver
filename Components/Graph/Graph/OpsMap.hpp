a30c794eb7b1e1b846b40c3d8d41db6a947267ca
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-04-06 00:23:15 +0200

    towards simple-tree

--- Components/Graph/Graph/OpsMap.hpp
+++ Components/Graph/Graph/OpsMap.hpp
@@ -86,167 +86,193 @@ MapOps::MapOps(mlir::ModuleOp module,
       }
 #endif
       if (auto frPrj = dyn_cast<mlir::quiccir::FrPOp>(op)) {
-        using namespace QuICC::Transform::Fourier;
-        using backend_t = QuICC::Graph::viewCpu_t;
-        using Tin = C_DCCSC3D_t;
-        using Tout = R_DCCSC3D_t;
-        using backendFft_t = Fft_t<backend_t, Tout, Tin>;
-        using backendDiff_t = MixedDiff_t<backend_t, Tin, 0, bwd_t,
-          QuICC::Transform::Fourier::none_m>;
-        using op_t = Mixed::Projector::DOp<Tout, Tin, backendFft_t,
-        backendDiff_t>;
-        _ops.push_back(std::make_unique<op_t>(mem));
-        // get index from MLIR source
+        // Get index from MLIR source
         std::uint64_t index = frPrj.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
-
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Transform::Fourier;
+          using backend_t = QuICC::Graph::viewCpu_t;
+          using Tin = C_DCCSC3D_t;
+          using Tout = R_DCCSC3D_t;
+          using backendFft_t = Fft_t<backend_t, Tout, Tin>;
+          using backendDiff_t = MixedDiff_t<backend_t, Tin, 0, bwd_t,
+            QuICC::Transform::Fourier::none_m>;
+          using op_t = Mixed::Projector::DOp<Tout, Tin, backendFft_t,
+          backendDiff_t>;
+          _ops.push_back(std::make_unique<op_t>(mem));
+          auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
+        }
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       else if (auto frInt = dyn_cast<mlir::quiccir::FrIOp>(op)) {
-        using namespace QuICC::Transform::Fourier;
-        using backend_t = QuICC::Graph::viewCpu_t;
-        using Tin = R_DCCSC3D_t;
-        using Tout = C_DCCSC3D_t;
-        using backendFft_t = Fft_t<backend_t, Tout, Tin>;
-        using backendDiff_t = MixedDiff_t<backend_t, Tout, 0, fwd_t,
-          QuICC::Transform::Fourier::none_m>;
-        using op_t = Mixed::Integrator::DOp<Tout, Tin, backendFft_t,
-        backendDiff_t>;
-        _ops.push_back(std::make_unique<op_t>());
-        // get index from MLIR source
+        // Get index from MLIR source
         std::uint64_t index = frInt.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
-
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Transform::Fourier;
+          using backend_t = QuICC::Graph::viewCpu_t;
+          using Tin = R_DCCSC3D_t;
+          using Tout = C_DCCSC3D_t;
+          using backendFft_t = Fft_t<backend_t, Tout, Tin>;
+          using backendDiff_t = MixedDiff_t<backend_t, Tout, 0, fwd_t,
+            QuICC::Transform::Fourier::none_m>;
+          using op_t = Mixed::Integrator::DOp<Tout, Tin, backendFft_t,
+          backendDiff_t>;
+          _ops.push_back(std::make_unique<op_t>());
+          auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
+        }
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       else if (auto alInt = dyn_cast<mlir::quiccir::AlIOp>(op)) {
-        using namespace QuICC::Transform::Quadrature;
-        using Tin = C_DCCSC3D_t;
-        using Tout = C_S1CLCSC3D_t;
-        using Top = QuICC::View::View<double, QuICC::View::S1CLCSC3DJIK>;
-        using backend_t = Cpu::ImplOp<Tout, Tin, Top>;
-        using op_t = Op<Tout, Tin, Top, backend_t>;
-        _ops.push_back(std::make_unique<op_t>(mem));
         // Get index from MLIR source
         std::uint64_t index = alInt.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
-        // Setup operator
-        auto alIntOp = dynamic_cast<op_t*>(ptr);
-        constexpr size_t rank = 3;
-        /// dim 0 - L  - harmonic degree
-        /// dim 1 - Ntheta - colatitudinal points
-        /// dim 2 - M  - harmonic order
-        std::array<std::uint32_t, rank> dims {modsDims[1], physDims[1], modsDims[0]};
-        std::vector<std::uint32_t> layers;
-        // Dense operator \todo generalize for distributed op
-        for (std::size_t i = 0; i < dims[2]; ++i) {
-          layers.push_back(i);
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Transform::Quadrature;
+          using Tin = C_DCCSC3D_t;
+          using Tout = C_S1CLCSC3D_t;
+          using Top = QuICC::View::View<double, QuICC::View::S1CLCSC3DJIK>;
+          using backend_t = Cpu::ImplOp<Tout, Tin, Top>;
+          using op_t = Op<Tout, Tin, Top, backend_t>;
+          _ops.push_back(std::make_unique<op_t>(mem));
+          auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
+          // Setup operator
+          auto alIntOp = dynamic_cast<op_t*>(ptr);
+          constexpr size_t rank = 3;
+          /// dim 0 - L  - harmonic degree
+          /// dim 1 - Ntheta - colatitudinal points
+          /// dim 2 - M  - harmonic order
+          std::array<std::uint32_t, rank> dims {modsDims[1], physDims[1], modsDims[0]};
+          std::vector<std::uint32_t> layers;
+          // Dense operator \todo generalize for distributed op
+          for (std::size_t i = 0; i < dims[2]; ++i) {
+            layers.push_back(i);
+          }
+          alIntOp->allocOp(dims, layers);
+          // Set grid \todo set once per operator kind
+          Internal::Array igrid;
+          Internal::Array iweights;
+          ::QuICC::Polynomial::Quadrature::LegendreRule quad;
+          quad.computeQuadrature(igrid, iweights, dims[1]);
+          // Populate op
+          auto opView = alIntOp->getOp();
+          using namespace QuICC::Transform::ALegendre;
+          builder<Top, Polynomial::ALegendre::Plm, Internal::Array::Scalar, 0>(opView, igrid, iweights);
+          // Add to thisArr
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
         }
-        alIntOp->allocOp(dims, layers);
-        // Set grid \todo set once per operator kind
-        Internal::Array igrid;
-        Internal::Array iweights;
-        ::QuICC::Polynomial::Quadrature::LegendreRule quad;
-        quad.computeQuadrature(igrid, iweights, dims[1]);
-        // Populate op
-        auto opView = alIntOp->getOp();
-        using namespace QuICC::Transform::ALegendre;
-        builder<Top, Polynomial::ALegendre::Plm, Internal::Array::Scalar, 0>(opView, igrid, iweights);
-        // Add to thisArr
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
-
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       else if (auto jwInt = dyn_cast<mlir::quiccir::JWIOp>(op)) {
-        using namespace QuICC::Transform::Quadrature;
-        using Tin = C_DCCSC3D_t;
-        using Tout = C_DCCSC3D_t;
-        using Top = QuICC::View::View<double, QuICC::View::CSL3DJIK>;
-        using backend_t = Cpu::ImplOp<Tout, Tin, Top>;
-        using op_t = Op<Tout, Tin, Top, backend_t>;
-        _ops.push_back(std::make_unique<op_t>(mem));
         // Get index from MLIR source
         std::uint64_t index = jwInt.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
-        // Setup operator
-        auto jwIntOp = dynamic_cast<op_t*>(ptr);
-        constexpr size_t rank = 3;
-        // dim 0 - N  - radial modes
-        // dim 1 - Nr - radial points
-        // dim 2 - L  - harmonic degree
-        std::array<std::uint32_t, rank> dims {modsDims[2], physDims[2], modsDims[1]};
-        std::vector<std::uint32_t> layers;
-        /// Dense operator \todo generalize for distributed op
-        for (std::size_t i = 0; i < dims[2]; ++i) {
-          layers.push_back(i);
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Transform::Quadrature;
+          using Tin = C_DCCSC3D_t;
+          using Tout = C_DCCSC3D_t;
+          using Top = QuICC::View::View<double, QuICC::View::CSL3DJIK>;
+          using backend_t = Cpu::ImplOp<Tout, Tin, Top>;
+          using op_t = Op<Tout, Tin, Top, backend_t>;
+          _ops.push_back(std::make_unique<op_t>(mem));
+          auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
+          // Setup operator
+          auto jwIntOp = dynamic_cast<op_t*>(ptr);
+          constexpr size_t rank = 3;
+          // dim 0 - N  - radial modes
+          // dim 1 - Nr - radial points
+          // dim 2 - L  - harmonic degree
+          std::array<std::uint32_t, rank> dims {modsDims[2], physDims[2], modsDims[1]};
+          std::vector<std::uint32_t> layers;
+          /// Dense operator \todo generalize for distributed op
+          for (std::size_t i = 0; i < dims[2]; ++i) {
+            layers.push_back(i);
+          }
+          jwIntOp->allocOp(dims, layers);
+          // Set grid \todo set once per operator kind
+          Internal::Array igrid;
+          Internal::Array iweights;
+          ::QuICC::Polynomial::Quadrature::WorlandRule quad;
+          quad.computeQuadrature(igrid, iweights, dims[1]);
+          // Populate op
+          auto opView = jwIntOp->getOp();
+          using namespace QuICC::Polynomial::Worland;
+          using namespace QuICC::Transform::Worland;
+          Builder<Top, QuICC::DenseSM::Worland::Operator<Wnl>, fwd_t> tBuilder;
+          tBuilder.compute(opView, igrid, iweights);
+          // Add to thisArr
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
         }
-        jwIntOp->allocOp(dims, layers);
-        // Set grid \todo set once per operator kind
-        Internal::Array igrid;
-        Internal::Array iweights;
-        ::QuICC::Polynomial::Quadrature::WorlandRule quad;
-        quad.computeQuadrature(igrid, iweights, dims[1]);
-        // Populate op
-        auto opView = jwIntOp->getOp();
-        using namespace QuICC::Polynomial::Worland;
-        using namespace QuICC::Transform::Worland;
-        Builder<Top, QuICC::DenseSM::Worland::Operator<Wnl>, fwd_t> tBuilder;
-        tBuilder.compute(opView, igrid, iweights);
-        // Add to thisArr
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       else if (auto add = dyn_cast<mlir::quiccir::AddOp>(op)) {
-        using namespace QuICC::Pointwise::Cpu;
-        using namespace QuICC::Pointwise;
-        using T = C_DCCSC3D_t;
-        using op_t = Op<AddFunctor<std::complex<double>>, T, T, T>;
-        _ops.push_back(std::make_unique<op_t>(AddFunctor<std::complex<double>>()));
-        // get index from MLIR source
+        // Get index from MLIR source
         std::uint64_t index = add.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<NaryOp<C_DCCSC3D_t, C_DCCSC3D_t, C_DCCSC3D_t>>>(_ops.back()).get();
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Pointwise::Cpu;
+          using namespace QuICC::Pointwise;
+          using T = C_DCCSC3D_t;
+          using op_t = Op<AddFunctor<std::complex<double>>, T, T, T>;
+          _ops.push_back(std::make_unique<op_t>(AddFunctor<std::complex<double>>()));
+          auto* ptr = std::get<std::shared_ptr<NaryOp<C_DCCSC3D_t, C_DCCSC3D_t, C_DCCSC3D_t>>>(_ops.back()).get();
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
+        }
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       else if (auto tran = dyn_cast<mlir::quiccir::TransposeOp>(op)) {
-        using namespace QuICC::Transpose::Cpu;
-        using namespace QuICC::Transpose;
-        // check type attribute
-        using Tin = C_DCCSC3D_t;
-        using Tout = C_DCCSC3D_t;
-        // check perm attribute
-        auto perm = tran.getPermutation();
-        assert(perm[0] == 2);
-        assert(perm[1] == 0);
-        assert(perm[2] == 1);
-        using op_t = Op<Tout, Tin, p201_t>;
-        _ops.push_back(std::make_unique<op_t>());
-        // get index from MLIR source
+        // Get index from MLIR source
         std::uint64_t index = tran.getImplptr().value();
         if (index >= _thisArr.size()) {
-          _thisArr.resize(index+1);
+          _thisArr.resize(index+1, nullptr);
         }
-        auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
-        assert(ptr != nullptr);
-        _thisArr[index] = ptr;
-
+        if (_thisArr[index] == nullptr) {
+          using namespace QuICC::Transpose::Cpu;
+          using namespace QuICC::Transpose;
+          // check type attribute
+          using Tin = C_DCCSC3D_t;
+          using Tout = C_DCCSC3D_t;
+          // check perm attribute
+          auto perm = tran.getPermutation();
+          assert(perm[0] == 2);
+          assert(perm[1] == 0);
+          assert(perm[2] == 1);
+          using op_t = Op<Tout, Tin, p201_t>;
+          _ops.push_back(std::make_unique<op_t>());
+          auto* ptr = std::get<std::shared_ptr<UnaryOp<Tout, Tin>>>(_ops.back()).get();
+          assert(ptr != nullptr);
+          _thisArr[index] = ptr;
+        }
+        else {
+          std::cout << "operator already allocated\n";
+        }
       }
       // return deallocateBuffers(op);
     //   if (failed(deallocateBuffers(op)))
