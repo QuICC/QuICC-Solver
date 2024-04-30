e459cbcdd9e5fa24b4a5bfd5ce6b6a1b23345e0f
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-04-30 10:31:20 +0200

    Add JLF scheme to ParaView reader

--- Scripts/ParaView/PythonQuICCReader.py
+++ Scripts/ParaView/PythonQuICCReader.py
@@ -68,7 +68,7 @@ class PythonQuICCReaderBase(VTKPythonAlgorithmBase):
         self._scheme = self._current_file['/'].attrs['type']
 
         # Spherical schemes
-        if self._scheme in [b'SLFl', b'SLFm', b'WLFl', b'WLFm']:
+        if self._scheme in [b'SLFl', b'SLFm', b'WLFl', b'WLFm', b'JLFl', b'JLFm']:
             self._components = ['r', 'theta', 'phi']
             self._make_mesh_coordinates = self._spherical_mesh
             self._vcomp = dict({'x':self._spherical_x_vcomp, 'y':self._spherical_y_vcomp, 'z':self._spherical_z_vcomp})
