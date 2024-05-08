2db098c839d3c85bf954bab60f135685825a4b4f
Philippe Marti (garulfo) <philippe.marti@colorado.edu>
2024-05-08 12:15:28 +0200

    Bessel boundary conditions with tests

---
+++ Components/SparseSM/src/Bessel/Boundary/Value.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file Value.cpp
+ * @brief Source of the implementation of boundary value
+ */
+
+// System includes
+//
+
+// Project includes
+//
+#include "QuICC/SparseSM/Bessel/Boundary/Value.hpp"
+#include "Types/Internal/BasicTypes.hpp"
+#include "Types/Typedefs.hpp"
+#include "Polynomial/SphericalBessel/Jnl.hpp"
+#include "Types/Internal/Literals.hpp"
+
+namespace QuICC {
+
+namespace SparseSM {
+
+namespace Bessel {
+
+namespace Boundary {
+
+   Value::Value(const BesselKind type, const int l)
+      : ICondition(type, l)
+   {
+   }
+
+   Value::ACoeff_t Value::compute(const int maxN)
+   {
+      using namespace Internal::Literals;
+
+      ACoeff_t val = ACoeff_t::Ones(maxN+1);
+      Internal::MHDFloat dNu;
+      if(this->type() == BesselKind::VALUE)
+      {
+         dNu = Polynomial::SphericalBessel::Value_dNu();
+      }
+      else if(this->type() == BesselKind::INSULATING)
+      {
+         dNu = Polynomial::SphericalBessel::Insulating_dNu();
+      }
+      else
+      {
+         throw std::logic_error("Unknown Bessel Kind");
+      }
+
+      std::vector<Internal::MHDFloat> roots;
+      int il = static_cast<int>(this->l());
+      Polynomial::SphericalBessel::getRoots(roots, il, maxN+1, dNu);
+      for(int i = 0; i < val.size(); i++)
+      {
+         const auto& k = roots.at(i);
+         val(i) = Polynomial::SphericalBessel::SphJnl(k, il, 1_mp, dNu);
+      }
+
+      return val;
+   }
+
+} // Boundary
+} // Bessel
+} // SparseSM
+} // QuICC
