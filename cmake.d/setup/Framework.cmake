80f54fed77a4c9c122517c9594e48c73aac6062e
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-07-24 18:20:17 +0200

    remove cmake QUICC_MPI

--- cmake.d/setup/Framework.cmake
+++ cmake.d/setup/Framework.cmake
@@ -23,54 +23,6 @@ quicc_add_definition(QUICC_MEMORYUSAGE)
 
 
 ###################################################
-#------------- MPI PARALLELISATION ---------------#
-###################################################
-
-#
-# Enable MPI parallelisation
-#
-option(QUICC_USE_MPI "Use MPI algorithm" OFF)
-
-if(QUICC_USE_MPI)
-   if(NOT MPI_FOUND)
-      message(SEND_ERROR "QUICC_USE_MPI requires MPI.")
-   endif(NOT MPI_FOUND)
-   set(QUICC_MPI ON)
-   add_definitions("-DQUICC_MPI")
-   option(QUICC_MPI_CI "Running benchmark on the CI?" OFF)
-   mark_as_advanced(QUICC_MPI_CI)
-else()
-   set(QUICC_MPI OFF)
-endif()
-
-
-###################################################
-#--------------- MPI DATA PACKING ----------------#
-###################################################
-
-if(QUICC_MPI)
-   quicc_create_option(NAME QUICC_MPIPACK
-                       OPTS "MPI" "Manual"
-                       LABEL "MPI data packing"
-                       ADVANCED)
-   quicc_add_definition(QUICC_MPIPACK)
-endif(QUICC_MPI)
-
-
-###################################################
-#--------------- MPI COMMUNICATION ---------------#
-###################################################
-
-if(QUICC_MPI)
-   quicc_create_option(NAME QUICC_MPICOMM
-                       OPTS "AllToAll" "SendRecv"
-                       LABEL "MPI communication"
-                       ADVANCED)
-   quicc_add_definition(QUICC_MPICOMM)
-endif(QUICC_MPI)
-
-
-###################################################
 #------------------- PROFILING -------------------#
 ###################################################
 quicc_create_option(NAME QUICC_PROFILE_BACKEND
