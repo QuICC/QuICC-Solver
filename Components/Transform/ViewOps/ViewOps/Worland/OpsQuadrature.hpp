d7a7ec01f4d3b687284c5845a952cd6d48a838cf
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-08-13 17:27:55 +0200

    WIP JW reductors

--- Components/Transform/ViewOps/ViewOps/Worland/OpsQuadrature.hpp
+++ Components/Transform/ViewOps/ViewOps/Worland/OpsQuadrature.hpp
@@ -35,13 +35,66 @@ struct OpsQuadratureMap {
 template <class TAG>
 using OpsQuadrature = typename details::OpsQuadratureMap<TAG>::type;
 
-// // Specialized mapping for reductors
-// namespace details {
+// Specialized mapping for reductors
+namespace details {
 
-// using namespace QuICC::Polynomial::Worland;
+using namespace QuICC::Polynomial::Worland;
+
+/// @brief Compute quadrature for grid needed for power
+/// computations
+/// @param igrid
+/// @param iweights
+/// @param gSize
+/// @param shift
+template<int SHIFT>
+void computePowerQuadrature(Internal::Array& igrid, Internal::Array& iweights, const int gSize)
+{
+    int nrgSize = gSize + 2*SHIFT;
+    Polynomial::Quadrature::WorlandSphEnergyRule wquad;
+    wquad.computeQuadrature(igrid, iweights, nrgSize);
+}
+
+/// @brief Generic helper in order to avoid having to pass
+/// extra parameters to energy integrators
+template <class OP>
+struct EnergyHelperQuadMap;
+
+/// @brief Generic helper in order to avoid having to pass
+/// extra parameters to energy integrators
+template <>
+struct EnergyHelperQuadMap<EnergyD1R1_t>
+{
+    void computeQuadrature(Internal::Array& igrid, Internal::Array& iweights, const int gSize)
+    {
+        computePowerQuadrature<0>(igrid, iweights, gSize);
+    }
+};
+
+/// @brief Generic helper in order to avoid having to pass
+/// extra parameters to energy integrators
+template <>
+struct EnergyHelperQuadMap<EnergyR2_t>
+{
+    void computeQuadrature(Internal::Array& igrid, Internal::Array& iweights, const int gSize)
+    {
+        computePowerQuadrature<1>(igrid, iweights, gSize);
+    }
+};
+
+/// @brief Quadrature for EnergyD1R1
+template <>
+struct OpsQuadratureMap<EnergyD1R1_t> {
+    using type = EnergyHelperQuadMap<EnergyD1R1_t>;
+};
+
+/// @brief Quadrature for EnergyR2
+template <>
+struct OpsQuadratureMap<EnergyR2_t> {
+    using type = EnergyHelperQuadMap<EnergyR2_t>;
+};
 
 
-// } // namespace details
+} // namespace details
 
 } // namespace Worland
 } // namespace Transform
