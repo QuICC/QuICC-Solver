1ef3ba7760e5b7c2b8220b19307b60601d480630
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-08-23 15:54:03 +0200

    fix after merge

--- Components/Environment/Environment/MpiTypes.hpp
+++ Components/Environment/Environment/MpiTypes.hpp
@@ -24,77 +24,78 @@ namespace QuICC {
 
 namespace Environment {
 
+/**
+ * @brief This class implements a few type conversion routines for the MPI
+ * library
+ */
+struct MpiTypes
+{
    /**
-    * @brief This class implements a few type conversion routines for the MPI library
+    * @brief Get MPI predefined type
     */
-   struct MpiTypes
-   {
-      /**
-       * @brief Get MPI predefined type
-       */
-      template <typename T> static MPI_Datatype type();
-   };
+   template <typename T> static MPI_Datatype type();
+};
 
-   /**
-    * @brief Specialised method for integer type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<int>()
-   {
-      return MPI_INT;
-   }
-
-   /**
-    * @brief Specialised method for long type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<long>()
-   {
-      return MPI_LONG;
-   }
-
-   /**
-    * @brief Specialised method for unsigned integer type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<unsigned int>()
-   {
-      return MPI_UNSIGNED;
-   }
-
-   /**
-    * @brief Specialised method for unsigned long type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<unsigned long>()
-   {
-      return MPI_UNSIGNED_LONG;
-   }
-
-   /**
-    * @brief Specialised method for float type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<float>()
-   {
-      return MPI_FLOAT;
-   }
-
-   /**
-    * @brief Specialised method for double type
-    */
-   template <> inline MPI_Datatype MpiTypes::type<double>()
-   {
-      return MPI_DOUBLE;
-   }
-
-   /**
-    * @brief Specialised method for a complex<double>
-    */
-   template <> inline MPI_Datatype MpiTypes::type<std::complex<double> >()
-   {
-      return MPI_DOUBLE_COMPLEX;
-   }
-
+/**
+ * @brief Specialised method for integer type
+ */
+template <> inline MPI_Datatype MpiTypes::type<int>()
+{
+   return MPI_INT;
 }
+
+/**
+ * @brief Specialised method for long type
+ */
+template <> inline MPI_Datatype MpiTypes::type<long>()
+{
+   return MPI_LONG;
 }
+
+/**
+ * @brief Specialised method for unsigned integer type
+ */
+template <> inline MPI_Datatype MpiTypes::type<unsigned int>()
+{
+   return MPI_UNSIGNED;
+}
+
+/**
+ * @brief Specialised method for unsigned long type
+ */
+template <> inline MPI_Datatype MpiTypes::type<unsigned long>()
+{
+   return MPI_UNSIGNED_LONG;
+}
+
+/**
+ * @brief Specialised method for float type
+ */
+template <> inline MPI_Datatype MpiTypes::type<float>()
+{
+   return MPI_FLOAT;
+}
+
+/**
+ * @brief Specialised method for double type
+ */
+template <> inline MPI_Datatype MpiTypes::type<double>()
+{
+   return MPI_DOUBLE;
+}
+
+/**
+ * @brief Specialised method for a complex<double>
+ */
+template <> inline MPI_Datatype MpiTypes::type<std::complex<double>>()
+{
+   return MPI_DOUBLE_COMPLEX;
+}
+
+} // namespace Environment
+} // namespace QuICC
 
 #endif // QUICC_ENVIRONMENT_MPITYPES_HPP
 
 // Only define in MPI case
-#endif //QUICC_MPI
+#endif // QUICC_MPI
