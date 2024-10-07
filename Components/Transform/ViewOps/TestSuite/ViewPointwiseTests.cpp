0e7939f3b5e287079cc729010a391207d984f29e
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-10-07 14:25:01 +0200

    cross test

--- Components/Transform/ViewOps/TestSuite/ViewPointwiseTests.cpp
+++ Components/Transform/ViewOps/TestSuite/ViewPointwiseTests.cpp
@@ -154,3 +154,60 @@ TEST_CASE("Add Cpu Functor", "AddCpuFunctor")
       CHECK(viewOut[i] == val0 + val1);
    }
 }
+
+TEST_CASE("Cross Cpu Functor", "CrossCpuFunctor")
+{
+   auto constexpr S = 5;
+   // host mem block
+   QuICC::Memory::Cpu::NewDelete mem;
+   QuICC::Memory::MemBlock<double> memBlockU0(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockU1(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockU2(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockV0(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockV1(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockV2(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockC0(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockC1(S, &mem);
+   QuICC::Memory::MemBlock<double> memBlockC2(S, &mem);
+
+   // view in
+   using view_t = ViewBase<double>;
+   view_t viewU0(memBlockU0.data(), memBlockU0.size());
+   view_t viewU1(memBlockU1.data(), memBlockU1.size());
+   view_t viewU2(memBlockU2.data(), memBlockU2.size());
+   view_t viewV0(memBlockV0.data(), memBlockV0.size());
+   view_t viewV1(memBlockV1.data(), memBlockV1.size());
+   view_t viewV2(memBlockV2.data(), memBlockV2.size());
+
+   // view Cross
+   view_t viewC0(memBlockC0.data(), memBlockC0.size());
+   view_t viewC1(memBlockC1.data(), memBlockC1.size());
+   view_t viewC2(memBlockC2.data(), memBlockC2.size());
+
+   // init
+   for (std::uint64_t i = 0; i < S; ++i)
+   {
+      viewU0[i] = 1.6;
+      viewU1[i] = 2.5;
+      viewU2[i] = 3.4;
+      viewV0[i] = 4.3;
+      viewV1[i] = 5.2;
+      viewV2[i] = 6.1;
+   }
+
+   // Cross
+   double scaling = 0.25;
+   using namespace QuICC::Pointwise::Cpu;
+   using namespace QuICC::Pointwise;
+   auto addOp =
+      std::make_unique<Op<CrossCompFunctor<double>, view_t, view_t, view_t, view_t, view_t>>(
+         CrossCompFunctor<double>(scaling));
+
+   addOp->apply(viewC0, viewU1, viewU2, viewV1, viewV2);
+
+   // check
+   for (std::uint64_t i = 0; i < S; ++i)
+   {
+      CHECK(viewC0[i] == scaling * (viewU1[i] * viewV2[i] - viewU2[i] * viewV1[i]));
+   }
+}
