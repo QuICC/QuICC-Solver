795dc10dcb96bac07b3c2a66d90fe3b87e7a0a0d
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-10-17 15:45:29 +0200

    forgot memory pool files

---
+++ Components/Memory/Memory/Cpu/Pool.cpp
@@ -0,0 +1,125 @@
+/**
+ * @file Pool.cpp
+ * @brief
+ */
+
+// External includes
+//
+#include <cstddef>
+#ifndef NDEBUG
+#include <iostream>
+#endif
+#include <new>
+#include <cassert>
+
+// Project includes
+//
+#include "Pool.hpp"
+
+namespace QuICC {
+namespace Memory {
+namespace Cpu {
+
+Pool::~Pool()
+{
+    // deallocate all blocks
+    for(std::uint32_t i = 0; i < _blocks.size(); ++i)
+    {
+        if(_blocks[i].ptr != nullptr)
+        {
+            ::operator delete(_blocks[i].ptr, _blocks[i].blockSize, static_cast<std::align_val_t>(_alignment));
+#ifndef NDEBUG
+            std::cout << "deallocated: " << _blocks[i].blockSize << "\t@: " << _blocks[i].ptr << '\n';
+#endif
+            _blocks[i].ptr = nullptr;
+        }
+    }
+}
+
+void* Pool::do_allocate(std::size_t bytes, std::size_t)
+{
+    // update max block size
+    _maxBlockSize = std::max(bytes, _maxBlockSize);
+
+    std::uint32_t i = _blocks.size();
+    // search for free block
+    if(!_freeBlocks.empty())
+    {
+        // there is at least a free block
+        i = _freeBlocks.top();
+        _freeBlocks.pop();
+    }
+
+    // check if we run out of blocks
+    if(i == _blocks.size())
+    {
+        _blocks.push_back(details::block{});
+    }
+
+    // check if it was allocated before
+    if(_blocks[i].ptr != nullptr)
+    {
+        // check if big enough
+        if (bytes > _blocks[i].blockSize)
+        {
+            // realloc
+            ::operator delete(_blocks[i].ptr, _blocks[i].blockSize, static_cast<std::align_val_t>(_alignment));
+            _blocks[i].ptr = ::operator new(_maxBlockSize, static_cast<std::align_val_t>(_alignment));
+            _blocks[i].blockSize = _maxBlockSize;
+#ifndef NDEBUG
+            std::cout << "realloc block: " << i << "\tof size: " << _blocks[i].blockSize << '\n';
+#endif
+        }
+        else
+        {
+#ifndef NDEBUG
+            std::cout << "reuse block: " << i << "\tof size: " << _blocks[i].blockSize << '\n';
+#endif
+        }
+        _blocks[i].isValid = true;
+        return _blocks[i].ptr;
+    }
+
+    // if not allocate
+    auto ptr = ::operator new(_maxBlockSize, static_cast<std::align_val_t>(_alignment));
+    _blocks[i].blockSize = _maxBlockSize;
+#ifndef NDEBUG
+    std::cout << "allocated: " << _blocks[i].blockSize  << "\t@: " << ptr << '\n';
+#endif
+
+    // store
+    _blocks[i].ptr = ptr;
+    _blocks[i].isValid = true;
+    return ptr;
+}
+
+void Pool::do_deallocate(void* ptr, std::size_t bytes, std::size_t)
+{
+    assert(ptr != nullptr);
+
+    for(std::uint32_t i = 0; i < _blocks.size(); ++i)
+    {
+        if(_blocks[i].ptr == ptr)
+        {
+            _blocks[i].isValid = false;
+            _freeBlocks.push(i);
+#ifndef NDEBUG
+            std::cout << "freed block: " << i << "\tof size: " << _blocks[i].blockSize <<'\n';
+#endif
+            return;
+        }
+    }
+
+}
+
+bool Pool::do_is_equal(const QuICC::Memory::memory_resource& other) const noexcept
+{
+    if( this == &other ) return true;
+    const auto* const op = dynamic_cast<const Pool*>(&other);
+    return op != nullptr;
+}
+
+} // namespace Cpu
+} // namespace Memory
+} // namespace QuICC
+
---
+++ Components/Memory/Memory/Cpu/Pool.hpp
@@ -0,0 +1,97 @@
+/**
+ * @file Pool.hpp
+ * @brief
+ */
+
+#pragma once
+
+// External includes
+//
+#include <utility>
+#include <vector>
+#include <stack>
+#include <cstdint>
+
+// Project includes
+//
+#include "Memory/MemoryResource.hpp"
+
+namespace QuICC {
+namespace Memory {
+/// @brief This namespace provides cpu only memory_resources
+namespace Cpu {
+
+
+namespace details
+{
+    /// @brief block of the memory pool
+    struct block
+    {
+        /// @brief pointer to memory block
+        /// 8 bytes
+        void* ptr = nullptr;
+        /// @brief block size in bytes
+        /// 4 bytes
+        std::uint32_t blockSize = 0;
+        /// @brief flag to check if in use
+        /// 1 byte
+        bool isValid = true;
+    };
+
+} // namespace details
+
+
+/**
+ *  @brief This class provides a concrete implementation of a memory_resources.
+ * It is a simple memory pool implementation.
+ * Alignement is fixed to cache line size.
+ * The block size is grown dynamically to fit largest allocation.
+ *
+ * */
+class Pool : public memory_resource
+{
+public:
+    /// @brief ctor
+    explicit Pool() = default;
+    /// @brief dtor
+    ~Pool();
+    /// @brief delete copy ctor
+    /// @param
+    Pool (const Pool&) = delete;
+    /// @brief delete assignment operator
+    /// @param
+    /// @return
+    Pool& operator= (const Pool&) = delete;
+protected:
+    /// @brief allocates memory
+    /// @param bytes  size in bytes of the memory to be allocated
+    /// @param alignment optional alignment in bytes
+    /// @return pointer to allocated memory
+    void* do_allocate(std::size_t bytes, std::size_t alignment) override;
+
+    /// @brief deallocates memory
+    /// @param p pointer to memory
+    /// @param bytes  size in bytes of the memory to be allocated
+    /// @param alignment optional alignment in bytes
+    void do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) override;
+
+    /// @brief compare for equality with another memory_resource
+    /// @param other ref to memory resource
+    /// @return true if equivalent
+    bool do_is_equal(const QuICC::Memory::memory_resource& other) const noexcept override;
+
+    /// @brief memory blocks storage
+    std::vector<details::block> _blocks;
+    /// @brief stack keeping track of free blocks indeces
+    std::stack<std::uint32_t> _freeBlocks;
+    /// @brief alignement in bytes, cache line
+    const std::uint32_t _alignment = 64;
+    /// @brief current biggest block size
+    std::size_t _maxBlockSize = 128;
+
+};
+
+} // namespace Cpu
+} // namespace Memory
+} // namespace QuICC
+
