a741d4b13eb7d981378489d251e2b7e208454121
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-11-28 11:26:36 +0100

    Fix references

--- cmake.d/FetchBenchmarkReference.cmake
+++ cmake.d/FetchBenchmarkReference.cmake
@@ -28,7 +28,7 @@ function(quicc_fetch_benchmark_reference target)
   message(DEBUG "QFB_DATADIR: ${QFB_DATADIR}")
 
   if(NOT QFB_GITTAG)
-    set(QFB_GITTAG "v0.7.0")
+    set(QFB_GITTAG "new/rrbc_stability")
   endif()
   message(DEBUG "QFB_GITTAG: ${QFB_GITTAG}")
 
