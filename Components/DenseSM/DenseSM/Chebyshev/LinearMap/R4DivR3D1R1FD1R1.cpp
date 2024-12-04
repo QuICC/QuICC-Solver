6c3bf2643613f23108a57b2c294890e1fe043070
Philippe Marti <philippe.marti@erdw.ethz.ch>
2024-12-04 15:22:02 +0100

    Last operator of this batch

---
+++ Components/DenseSM/DenseSM/Chebyshev/LinearMap/R4DivR3D1R1FD1R1.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file R4DivR3D1R1FD1R1.cpp
+ * @brief Source of the implementation of the spectral operator r^4 D(r f) D(r *)
+ */
+
+// System includes
+//
+#include <Eigen/Dense>
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+
+// Project includes
+//
+#include "DenseSM/Chebyshev/LinearMap/R4DivR3D1R1FD1R1.hpp"
+#include "QuICC/Transform/Fft/Chebyshev/LinearMap/Projector/D1Y1.hpp"
+#include "QuICC/Transform/Fft/Chebyshev/LinearMap/Integrator/P.hpp"
+#include "Types/Internal/Typedefs.hpp"
+
+namespace QuICC {
+
+namespace DenseSM {
+
+namespace Chebyshev {
+
+namespace LinearMap {
+
+R4DivR3D1R1FD1R1::R4DivR3D1R1FD1R1(const int nNr, const int nNc, const int lOut, const int lF, const int lIn,
+   std::shared_ptr<RadialTorPolFunction> pF, const Scalar_t lower, const Scalar_t upper) :
+    ITripleHarmonicOperator(nNr, nNc, lOut, lF, lIn, pF, lower, upper)
+{}
+
+void R4DivR3D1R1FD1R1::buildOpImpl(Internal::Matrix& mat, const int rows,
+   const int cols) const
+{
+   namespace cheb = Transform::Fft::Chebyshev::LinearMap;
+   typedef cheb::Integrator::P::SetupType SetupType;
+
+   const auto pId = GridPurpose::SIMULATION;
+   int rN = 2*this->cols();
+
+   // Compute grid
+   Internal::Array igrid, iweights;
+   this->computeQuadrature(igrid, iweights, rN);
+
+   auto sBwd = std::make_shared<SetupType>(rN, this->cols(), this->cols(), pId);
+   sBwd->setBounds(this->mcLower, this->mcUpper);
+   sBwd->lock();
+   cheb::Projector::D1Y1 TBwd;
+   TBwd.init(sBwd);
+
+   Matrix tmpA = Matrix::Identity(rN, this->cols());
+   Matrix tmpB = Matrix::Zero(rN, this->cols());
+   TBwd.transform(tmpB, tmpA);
+
+   Matrix fA = this->mpF->evaluate(igrid, this->mLf);
+   Matrix fB = Matrix::Zero(rN, 1);
+
+   auto sFFwd = std::make_shared<SetupType>(rN, 1, this->mpF->nN(), pId);
+   sFFwd->setBounds(this->mcLower, this->mcUpper);
+   sFFwd->lock();
+   cheb::Integrator::P TFFwd;
+   TFFwd.init(sFFwd);
+
+   TFFwd.transform(fB, fA);
+
+   auto sFBwd = std::make_shared<SetupType>(rN, 1, this->mpF->nN(), pId);
+   sFBwd->setBounds(this->mcLower, this->mcUpper);
+   sFBwd->lock();
+   cheb::Projector::D1Y1 TFBwd;
+   TFBwd.init(sFBwd);
+
+   TFBwd.transform(fA, fB);
+   fA = igrid.asDiagonal()*fA;
+
+   tmpB = fA.asDiagonal() * tmpB;
+
+   auto sFwd = std::make_shared<SetupType>(rN, this->cols(), this->rows(), pId);
+   sFwd->setBounds(this->mcLower, this->mcUpper);
+   sFwd->lock();
+   cheb::Integrator::P TFwd;
+   TFwd.init(sFwd);
+
+   TFwd.transform(tmpA, tmpB);
+
+   mat = tmpA.topRows(this->rows());
+}
+
+} // namespace LinearMap
+} // namespace Chebyshev
+} // namespace DenseSM
+} // namespace QuICC
---
+++ Components/DenseSM/DenseSM/Chebyshev/LinearMap/R4DivR3D1R1FD1R1.hpp
@@ -0,0 +1,71 @@
+/**
+ * @file R4DivR3D1R1FD1R1.hpp
+ * @brief Implementation of the spectral operator r^4 1/r^3 D(r f) D(r *)
+ */
+
+#ifndef QUICC_DENSESM_CHEBYSHEV_LINEARMAP_R4DIVR3D1R1FD1R1_HPP
+#define QUICC_DENSESM_CHEBYSHEV_LINEARMAP_R4DIVR3D1R1FD1R1_HPP
+
+// System includes
+//
+
+// Project includes
+//
+#include "DenseSM/Chebyshev/LinearMap/ITripleHarmonicOperator.hpp"
+#include "DenseSM/Chebyshev/LinearMap/RadialTorPolFunction.hpp"
+#include "Types/Internal/Typedefs.hpp"
+#include "Types/Typedefs.hpp"
+
+namespace QuICC {
+
+namespace DenseSM {
+
+namespace Chebyshev {
+
+namespace LinearMap {
+
+/**
+ * @brief Implementation of the spectral operator r^4 D(r f) D(r *)
+ */
+class R4DivR3D1R1FD1R1 : public ITripleHarmonicOperator
+{
+public:
+   /**
+    * @brief Constructor
+    *
+    * @param rows    Number of rows
+    * @param cols    Number of cols
+    * @param lOut    Output harmonic degree
+    * @param lF      harmonic degree of f
+    * @param lIn     Input harmonic degree
+    * @param lower   Lower boundar
+    * @param upper   Upper boundar
+    */
+   R4DivR3D1R1FD1R1(const int rows, const int cols, const int lOut, const int lF, const int lIn, std::shared_ptr<RadialTorPolFunction> pF, const Scalar_t lower,
+      const Scalar_t upper);
+
+   /**
+    * @brief Destructor
+    */
+   virtual ~R4DivR3D1R1FD1R1() = default;
+
+protected:
+   /**
+    * @brief Implementation of build dense matrix operator
+    *
+    * @param mat operator
+    * @param rows rows of matrix
+    * @param cols cols of matrix
+    */
+   void buildOpImpl(Internal::Matrix& mat, const int rows,
+      const int cols) const final;
+
+private:
+};
+
+} // namespace LinearMap
+} // namespace Chebyshev
+} // namespace DenseSM
+} // namespace QuICC
+
+#endif // QUICC_DENSESM_CHEBYSHEV_LINEARMAP_R4DIVR3D1R1FD1R1_HPP
