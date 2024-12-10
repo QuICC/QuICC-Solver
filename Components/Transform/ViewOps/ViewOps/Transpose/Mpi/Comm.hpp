0ebadea97206a9aa68752758fef2146c14788e2e
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-12-10 15:58:13 +0100

    cleanup

--- Components/Transform/ViewOps/ViewOps/Transpose/Mpi/Comm.hpp
+++ Components/Transform/ViewOps/ViewOps/Transpose/Mpi/Comm.hpp
@@ -100,149 +100,19 @@ public:
    Comm(MPI_Comm comm = MPI_COMM_WORLD) : _comm(comm){};
 
    /// @brief release Mpi resources
-   ~Comm()
-   {
-      for (std::size_t r = 0; r < _sendType.size(); ++r)
-      {
-         MPI_Type_free(&_sendType[r]);
-         MPI_Type_free(&_recvType[r]);
-      }
-   }
-
-   /// @brief Execute the data exchange (communication)
-   /// @param out
-   /// @param in
-   void exchange(TDATA* out, const TDATA* in)
-   {
-      if (_subComm != MPI_COMM_NULL)
-      {
-         if constexpr(std::is_same_v<TAG, alltoallw_t>)
-         {
-            MPI_Alltoallw(in, _sendCounts.data(), _sDispls.data(),
-               _sendType.data(), out, _recvCounts.data(), _rDispls.data(),
-               _recvType.data(), _subComm);
-         }
-         else
-         {
-            int nSubComm;
-            MPI_Status status;
-            MPI_Comm_size(_subComm, &nSubComm);
-            // Setup send/recv buffers
-            /// \todo gpu buffers
-            if (_sendBuffer.size() == 0)
-            {
-               // _sendBuffers.resize(nSubComm);
-               // _recvBuffers.resize(nSubComm);
-               // for (int i = 0; i < nSubComm; ++i)
-               // {
-               //    _sendBuffers[i] = std::vector<TDATA>(_sendDispls[i].size());
-               //    _recvBuffers[i] = std::vector<TDATA>(_recvDispls[i].size());
-               // }
-
-               // Get buffers size
-               _sendBufferDispls.resize(nSubComm+1);
-               _recvBufferDispls.resize(nSubComm+1);
-               _sendBufferDispls[0] = 0;
-               _recvBufferDispls[0] = 0;
-               for (int i = 1; i <= nSubComm; ++i)
-               {
-                  _sendBufferDispls[i] = _sendBufferDispls[i-1] + _sendCounts[i-1];
-                  _recvBufferDispls[i] = _recvBufferDispls[i-1] + _recvCounts[i-1];
-               }
-               _sendBuffer.resize(_sendBufferDispls[nSubComm]);
-               _recvBuffer.resize(_recvBufferDispls[nSubComm]);
-            }
-
-            // Pack
-            for (int i = 0; i < nSubComm; ++i)
-            {
-               for (int s = 0; s < _sendCounts[i]; ++s)
-               {
-                  // _sendBuffers[i][s] = *(in + _sendDispls[i][s]);
-                  _sendBuffer[_sendBufferDispls[i]+s] = *(in + _sendDispls[i][s]);
-               }
-            }
-
-            // Comm
-            if constexpr (std::is_same_v<TAG, sendrecv_t>)
-            {
-               for (int i = 0; i < nSubComm; ++i)
-               {
-                  // MPI_Send(_sendBuffers[i].data(), _sendBuffers[i].size(),
-                  //       Environment::MpiTypes::type<TDATA>(), i, /*tag*/0, _subComm);
-                  MPI_Send(_sendBuffer.data()+_sendBufferDispls[i], _sendCounts[i],
-                        Environment::MpiTypes::type<TDATA>(), i, /*tag*/1, _subComm);
-               }
-               for (int i = 0; i < nSubComm; ++i)
-               {
-                  // MPI_Recv(_recvBuffers[i].data(), _recvBuffers[i].size(),
-                  //       Environment::MpiTypes::type<TDATA>(), i, /*tag*/0, _subComm, &status);
-                  MPI_Recv(_recvBuffer.data()+_recvBufferDispls[i], _recvCounts[i],
-                        Environment::MpiTypes::type<TDATA>(), i, /*tag*/1, _subComm, &status);
-               }
-            }
-            else if constexpr (std::is_same_v<TAG, alltoallv_t>)
-            {
-               MPI_Alltoallv(_sendBuffer.data(), _sendCounts.data(),
-                  _sendBufferDispls.data(), Environment::MpiTypes::type<TDATA>(),
-                  _recvBuffer.data(), _recvCounts.data(),
-                  _recvBufferDispls.data(), Environment::MpiTypes::type<TDATA>(), _subComm);
-            }
-            else
-            {
-               throw std::logic_error("Comm not implemented");
-            }
-
-            // Unpack
-            for (int i = 0; i < nSubComm; ++i)
-            {
-               for (int s = 0; s < _recvCounts[i]; ++s)
-               {
-                  // *(out + _recvDispls[i][s]) = _recvBuffers[i][s];
-                  *(out + _recvDispls[i][s]) = _recvBuffer[_recvBufferDispls[i]+s];
-               }
-            }
-         }
-      }
-   }
+   ~Comm();
 
    /// @brief Set the communicator
    /// i.e. build the type for the MPI_Alltoallw exchange
    /// @param cooNew destination coordinates
    /// @param cooOld source coordinates
    void setComm(const std::vector<point_t>& cooNew,
-      const std::vector<point_t>& cooOld)
-   {
-      _sendDispls = getDispls(cooNew, cooOld);
-      _recvDispls = getDispls(cooOld, cooNew);
-      auto redSet = getReducedRanksSet(_sendDispls, _recvDispls);
-      redDisplsFromSet(_sendDispls, _recvDispls, redSet);
-      _subComm = QuICC::Transpose::Mpi::getSubComm(redSet);
-      auto subRanks = redSet.size();
-      // Build types
-      if (_subComm != MPI_COMM_NULL)
-      {
-         _sendType.resize(subRanks);
-         _recvType.resize(subRanks);
+      const std::vector<point_t>& cooOld);
 
-         for (std::size_t r = 0; r < subRanks; ++r)
-         {
-            MPI_Type_create_indexed_block(_sendDispls[r].size(), 1,
-               _sendDispls[r].data(), Environment::MpiTypes::type<TDATA>(),
-               &_sendType[r]);
-            MPI_Type_commit(&_sendType[r]);
-            MPI_Type_create_indexed_block(_recvDispls[r].size(), 1,
-               _recvDispls[r].data(), Environment::MpiTypes::type<TDATA>(),
-               &_recvType[r]);
-            MPI_Type_commit(&_recvType[r]);
-         }
-      }
-      _sendCounts = getCount<TAG>(_sendDispls);
-      _recvCounts = getCount<TAG>(_recvDispls);
-      _sDispls = std::vector<int>(subRanks, 0);
-      _rDispls = std::vector<int>(subRanks, 0);
-      _isSetup = true;
-   }
+   /// @brief Execute the data exchange (communication)
+   /// @param out
+   /// @param in
+   void exchange(TDATA* out, const TDATA* in);
 
    /// @brief check if the comm was setup
    /// @return
@@ -271,13 +141,6 @@ private:
    /// @brief Entry j specifies the displacement (in bytes, offset from recvbuf)
    /// to which data from rank j should be written.
    std::vector<int> _rDispls;
-
-   // /// @brief Send buffer for packed comms
-   // std::vector<std::vector<TDATA>> _sendBuffers;
-   // /// @brief Recv buffer for packed comms
-   // std::vector<std::vector<TDATA>> _recvBuffers;
-
-
    /// @brief Send buffer for packed comms
    std::vector<TDATA> _sendBuffer;
    /// @brief Recv buffer for packed comms
@@ -286,15 +149,147 @@ private:
    std::vector<int> _sendBufferDispls;
    /// @brief Recv buffer displacement for packed comms
    std::vector<int> _recvBufferDispls;
-
-
    /// @brief All world communicator
    MPI_Comm _comm;
    /// @brief Communicator over which data is to be exchanged.
    MPI_Comm _subComm;
+   /// @brief Sub communicator size
+   int _nSubComm;
    /// @brief Is comm setup?
    bool _isSetup = false;
 };
+
+
+template <class TDATA, class TAG>
+Comm<TDATA, TAG>::~Comm()
+{
+   if constexpr(std::is_same_v<TAG, alltoallw_t>)
+   {
+      for (std::size_t r = 0; r < _sendType.size(); ++r)
+      {
+         MPI_Type_free(&_sendType[r]);
+         MPI_Type_free(&_recvType[r]);
+      }
+   }
+}
+
+template <class TDATA, class TAG>
+void Comm<TDATA, TAG>::setComm(const std::vector<point_t>& cooNew,
+   const std::vector<point_t>& cooOld)
+{
+   _sendDispls = getDispls(cooNew, cooOld);
+   _recvDispls = getDispls(cooOld, cooNew);
+   auto redSet = getReducedRanksSet(_sendDispls, _recvDispls);
+   redDisplsFromSet(_sendDispls, _recvDispls, redSet);
+   _subComm = QuICC::Transpose::Mpi::getSubComm(redSet);
+   auto subRanks = redSet.size();
+   _sendCounts = getCount<TAG>(_sendDispls);
+   _recvCounts = getCount<TAG>(_recvDispls);
+   if (_subComm != MPI_COMM_NULL)
+   {
+      MPI_Comm_size(_subComm, &_nSubComm);
+      if constexpr(std::is_same_v<TAG, alltoallw_t>)
+      {
+         // Build types for alltoallw
+         _sendType.resize(subRanks);
+         _recvType.resize(subRanks);
+
+         for (std::size_t r = 0; r < subRanks; ++r)
+         {
+            MPI_Type_create_indexed_block(_sendDispls[r].size(), 1,
+               _sendDispls[r].data(), Environment::MpiTypes::type<TDATA>(),
+               &_sendType[r]);
+            MPI_Type_commit(&_sendType[r]);
+            MPI_Type_create_indexed_block(_recvDispls[r].size(), 1,
+               _recvDispls[r].data(), Environment::MpiTypes::type<TDATA>(),
+               &_recvType[r]);
+            MPI_Type_commit(&_recvType[r]);
+         }
+         _sDispls = std::vector<int>(subRanks, 0);
+         _rDispls = std::vector<int>(subRanks, 0);
+      }
+      else
+      {
+         // Setup send/recv buffers
+         /// \todo gpu buffers
+         _sendBufferDispls.resize(_nSubComm+1);
+         _recvBufferDispls.resize(_nSubComm+1);
+         _sendBufferDispls[0] = 0;
+         _recvBufferDispls[0] = 0;
+         for (int i = 1; i <= _nSubComm; ++i)
+         {
+            _sendBufferDispls[i] = _sendBufferDispls[i-1] + _sendCounts[i-1];
+            _recvBufferDispls[i] = _recvBufferDispls[i-1] + _recvCounts[i-1];
+         }
+         _sendBuffer.resize(_sendBufferDispls[_nSubComm]);
+         _recvBuffer.resize(_recvBufferDispls[_nSubComm]);
+      }
+   }
+   _isSetup = true;
+}
+
+template <class TDATA, class TAG>
+void Comm<TDATA, TAG>::exchange(TDATA* out, const TDATA* in)
+{
+   if (_subComm != MPI_COMM_NULL)
+   {
+      if constexpr(std::is_same_v<TAG, alltoallw_t>)
+      {
+         MPI_Alltoallw(in, _sendCounts.data(), _sDispls.data(),
+            _sendType.data(), out, _recvCounts.data(), _rDispls.data(),
+            _recvType.data(), _subComm);
+      }
+      else
+      {
+         // Pack
+         for (int i = 0; i < _nSubComm; ++i)
+         {
+            for (int s = 0; s < _sendCounts[i]; ++s)
+            {
+               // _sendBuffers[i][s] = *(in + _sendDispls[i][s]);
+               _sendBuffer[_sendBufferDispls[i]+s] = *(in + _sendDispls[i][s]);
+            }
+         }
+
+         // Comm
+         if constexpr (std::is_same_v<TAG, sendrecv_t>)
+         {
+            for (int i = 0; i < _nSubComm; ++i)
+            {
+               MPI_Send(_sendBuffer.data()+_sendBufferDispls[i], _sendCounts[i],
+                     Environment::MpiTypes::type<TDATA>(), i, /*tag*/1, _subComm);
+            }
+            MPI_Status status;
+            for (int i = 0; i < _nSubComm; ++i)
+            {
+               MPI_Recv(_recvBuffer.data()+_recvBufferDispls[i], _recvCounts[i],
+                     Environment::MpiTypes::type<TDATA>(), i, /*tag*/1, _subComm, &status);
+            }
+         }
+         else if constexpr (std::is_same_v<TAG, alltoallv_t>)
+         {
+            MPI_Alltoallv(_sendBuffer.data(), _sendCounts.data(),
+               _sendBufferDispls.data(), Environment::MpiTypes::type<TDATA>(),
+               _recvBuffer.data(), _recvCounts.data(),
+               _recvBufferDispls.data(), Environment::MpiTypes::type<TDATA>(), _subComm);
+         }
+         else
+         {
+            throw std::logic_error("Comm not implemented");
+         }
+
+         // Unpack
+         for (int i = 0; i < _nSubComm; ++i)
+         {
+            for (int s = 0; s < _recvCounts[i]; ++s)
+            {
+               *(out + _recvDispls[i][s]) = _recvBuffer[_recvBufferDispls[i]+s];
+            }
+         }
+      }
+   }
+}
+
 
 } // namespace Mpi
 } // namespace Transpose
