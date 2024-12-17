c0e0801d709df6b9285c81ca7d7db30191dfc023
Giacomo Castiglioni <giacomo.castiglioni@cscs.ch>
2024-12-17 14:52:10 +0100

    cuda packing not serial

--- Components/Transform/ViewOps/ViewOps/Transpose/Cuda/Packing.cu
+++ Components/Transform/ViewOps/ViewOps/Transpose/Cuda/Packing.cu
@@ -13,6 +13,8 @@
 #include "View/View.hpp"
 #include "Cuda/CudaUtil.hpp"
 
+#define SERIAL_PACKING 0
+
 namespace QuICC {
 /// @brief namespace for Transpose type operations
 namespace Transpose {
@@ -28,17 +30,29 @@ __global__ void pack(View::ViewBase<TDATA> buffer, const TDATA* in,
    const View::View<int, View::dense2DRM> sendDisplsView,
    const View::ViewBase<int> sendBufferDisplsView)
 {
-
-   const std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-   const std::size_t j = blockIdx.y * blockDim.y + threadIdx.y;
-
    const auto I = sendDisplsView.dims()[0];
    const auto J = sendDisplsView.dims()[1];
+
+   #if SERIAL_PACKING
+   for (size_t i = 0; i < I; ++i)
+   {
+      for (int j = 0; j < sendCountsView[i]; ++j)
+      {
+         buffer[sendBufferDisplsView[i]+j] = *(in + sendDisplsView[i*J+j]);
+      }
+   }
+   #else
+   const std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+   const std::size_t j = blockIdx.y * blockDim.y + threadIdx.y;
 
    if (i < I && j < sendCountsView[i])
    {
       buffer[sendBufferDisplsView[i]+j] = *(in + sendDisplsView[i*J+j]);
    }
+   #endif
+
+
+
 }
 
 } // namespace details
@@ -58,16 +72,26 @@ void pack(View::ViewBase<TDATA> buffer, const TDATA* in,
    dim3 blockSize;
    dim3 numBlocks;
 
+   #if SERIAL_PACKING
+   blockSize.x = 1;
+   blockSize.y = 1;
+   blockSize.z = 1;
+   numBlocks.x = 1;
+   numBlocks.y = 1;
+   numBlocks.z = 1;
+   #else
    blockSize.x = 16;
    blockSize.y = 64;
    blockSize.z = 1;
    numBlocks.x = (I + blockSize.x - 1) / blockSize.x;
    numBlocks.y = (J + blockSize.y - 1) / blockSize.y;
    numBlocks.z = 1;
+   #endif
 
    details::pack<TDATA>
-      <<<numBlocks, blockSize>>>(buffer, in , sendCountsView, sendDisplsView, sendBufferDisplsView);
+      <<<numBlocks, blockSize>>>(buffer, in, sendCountsView, sendDisplsView, sendBufferDisplsView);
 
+   cudaErrChk(cudaDeviceSynchronize());
 }
 
 
@@ -79,17 +103,27 @@ __global__ void unPack(TDATA* out, const View::ViewBase<TDATA> buffer,
    const View::View<int, View::dense2DRM> recvDisplsView,
    const View::ViewBase<int> recvBufferDisplsView)
 {
-
-   const std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-   const std::size_t j = blockIdx.y * blockDim.y + threadIdx.y;
-
    const auto I = recvDisplsView.dims()[0];
    const auto J = recvDisplsView.dims()[1];
+
+   #if SERIAL_PACKING
+   for (size_t i = 0; i < I; ++i)
+   {
+      for (int j = 0; j < recvCountsView[i]; ++j)
+      {
+         *(out + recvDisplsView[i*J+j]) = buffer[recvBufferDisplsView[i]+j];
+      }
+   }
+   #else
+   const std::size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+   const std::size_t j = blockIdx.y * blockDim.y + threadIdx.y;
 
    if (i < I && j < recvCountsView[i])
    {
       *(out + recvDisplsView[i*J+j]) = buffer[recvBufferDisplsView[i]+j];
    }
+   #endif
+
 }
 
 } // namespace details
@@ -109,16 +143,27 @@ void unPack(TDATA* out, const View::ViewBase<TDATA> buffer,
    dim3 blockSize;
    dim3 numBlocks;
 
+   #if SERIAL_PACKING
+   blockSize.x = 1;
+   blockSize.y = 1;
+   blockSize.z = 1;
+   numBlocks.x = 1;
+   numBlocks.y = 1;
+   numBlocks.z = 1;
+   #else
    blockSize.x = 16;
    blockSize.y = 64;
    blockSize.z = 1;
    numBlocks.x = (I + blockSize.x - 1) / blockSize.x;
    numBlocks.y = (J + blockSize.y - 1) / blockSize.y;
    numBlocks.z = 1;
+   #endif
+
 
    details::unPack<TDATA>
       <<<numBlocks, blockSize>>>(out, buffer, recvCountsView, recvDisplsView, recvBufferDisplsView);
 
+   cudaErrChk(cudaDeviceSynchronize());
 }
 
 // Explicit instantiations
