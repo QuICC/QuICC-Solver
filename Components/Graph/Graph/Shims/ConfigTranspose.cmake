b3eda223234a999bd2fb0c962b2d985f8567c41c
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2025-03-03 19:31:35 +0100

    towards generating shims

--- Components/Graph/Graph/Shims/ConfigTranspose.cmake
+++ Components/Graph/Graph/Shims/ConfigTranspose.cmake
@@ -4,10 +4,12 @@
 
 include(ConfigUtils.cmake)
 
-set(LayoutIns "DCCSC3D;")
-set(LayoutOuts "DCCSC3D;")
+set(LayoutIns "DCCSC3D")
+set(LayoutOuts "DCCSC3D")
 set(TypeIns "std::complex<double>")
 set(TypeOuts "std::complex<double>")
+
+set(GroupSizes "1;2")
 
 # Configure Shims
 foreach(LayoutIn IN LISTS LayoutIns)
@@ -16,18 +18,41 @@ foreach(LayoutIn IN LISTS LayoutIns)
             foreach(TypeOut IN LISTS TypeOuts)
                 mapType2mlir(${TypeIn} MlirTypeIn)
                 mapType2mlir(${TypeOut} MlirTypeOut)
-                camelCase(${TypeIn} KindIn)
-                camelCase(${TypeOut} KindOut)
-                camelCase(${LayoutIn} LayoutIn)
-                camelCase(${LayoutOut} LayoutOut)
-                configure_file(
-                    "MlirTransposeShims.cpp.in"
-                    "${CMAKE_BINARY_DIR}/${QUICC_CURRENT_COMPONENT_DIR}/Transpose/MlirShims/${KindIn}${KindOut}${LayoutIn}${LayoutOut}.cpp"
-                )
-                target_sources(${QUICC_CURRENT_COMPONENT_LIB}_${QUICC_CURRENT_SUBCOMPONENT_LIB}
-                    PRIVATE
-                        "${CMAKE_BINARY_DIR}/${QUICC_CURRENT_COMPONENT_DIR}/Transpose/MlirShims/${KindIn}${KindOut}${LayoutIn}${LayoutOut}.cpp"
-                )
+
+                foreach(GroupSize IN LISTS GroupSizes)
+                    math(EXPR GroupSizeM1 "${GroupSize}-1")
+                    set(FunName "_ciface_quiccir_transpose")
+                    foreach(It RANGE 0 ${GroupSizeM1})
+                        string(APPEND FunName "_${MlirTypeOut}_${LayoutOut}")
+                    endforeach()
+                    foreach(It RANGE 0 ${GroupSizeM1})
+                        string(APPEND FunName "_${MlirTypeIn}_${LayoutIn}")
+                    endforeach()
+
+                    set(FunSignature "${FunName}(void* obj")
+
+                    foreach(It RANGE 0 ${GroupSizeM1})
+                        string(APPEND FunSignature ", ViewDescriptor<${TypeOut}, std::uint32_t, 3>* pOut${It}")
+                    endforeach()
+
+                    foreach(It RANGE 0 ${GroupSizeM1})
+                        string(APPEND FunSignature ", const ViewDescriptor<${TypeIn}, std::uint32_t, 3>* pIn${It}")
+                    endforeach()
+                    string(APPEND FunSignature ")")
+
+                    # View defs
+
+                    configure_file(
+                        "MlirTransposeShims.cpp.in"
+                        "${CMAKE_BINARY_DIR}/${QUICC_CURRENT_COMPONENT_DIR}/Transpose/MlirShims/${FunName}.cpp"
+                    )
+                    target_sources(${QUICC_CURRENT_COMPONENT_LIB}_${QUICC_CURRENT_SUBCOMPONENT_LIB}
+                        PRIVATE
+                            "${CMAKE_BINARY_DIR}/${QUICC_CURRENT_COMPONENT_DIR}/Transpose/MlirShims/${FunName}.cpp"
+                    )
+                endforeach(GroupSize IN LISTS GroupSizes)
+
+
             endforeach()
         endforeach()
     endforeach()
