d537f90099cf0706507429214ff05afcb27cb614
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-03-11 12:48:02 +0100

    Add new polynomial operator

---
+++ Components/Polynomial/include/QuICC/Polynomial/Worland/r_1dWnl.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file r_1dWnl.hpp
+ * @brief Implementation of the 1/r D Worland polynomial
+ */
+
+#ifndef QUICC_POLYNOMIAL_WORLAND_R_1DWNL_HPP
+#define QUICC_POLYNOMIAL_WORLAND_R_1DWNL_HPP
+
+// System includes
+//
+
+// Project includes
+//
+#include "QuICC/Polynomial/Worland/r_1dWnlRecurrence.hpp"
+
+#endif // QUICC_POLYNOMIAL_WORLAND_R_1DWNL_HPP
