c015e1293ce161707080905d946350e33e541438
Giacomo Castiglioni <giacastiglioni@gmail.com>
2025-03-25 10:48:17 +0100

    unformat

--- Components/Io/include/QuICC/Io/Xml/IXmlReader.hpp
+++ Components/Io/include/QuICC/Io/Xml/IXmlReader.hpp
@@ -10,8 +10,8 @@
 //
 #include <fstream>
 #include <sstream>
-#include <stdexcept>
 #include <vector>
+#include <stdexcept>
 
 // External includes
 //
@@ -27,125 +27,120 @@ namespace Io {
 
 namespace Xml {
 
-/**
- * @brief Interface to and XML reader (based on rapidXML)
- */
-class IXmlReader : public XmlFile
-{
-public:
    /**
-    * @brief Constructor
-    *
-    * @param name     Filename
-    * @param ext      File extension
-    * @param header   Header string of file
-    * @param type     Type string of the file
-    * @param version  Version string of the file
-    * @param root     XML root string of the file
+    * @brief Interface to and XML reader (based on rapidXML)
     */
-   IXmlReader(std::string name, std::string ext, std::string header,
-      std::string type, std::string version, std::string root);
-
-   /**
-    * @brief Destructor
-    */
-   virtual ~IXmlReader();
-
-   /**
-    * @brief Initialise the file
-    */
-   virtual void init();
-
-   /**
-    * @brief Read the content
-    */
-   virtual void read() = 0;
-
-   /**
-    * @brief Finalise the file
-    */
-   virtual void finalize();
-
-protected:
-   /**
-    * @brief Handle to the file
-    */
-   std::ifstream mFile;
-
-   /**
-    * @brief Content of file converted into a vector
-    */
-   std::vector<char> mContent;
-
-   /**
-    * @brief Open the file
-    */
-   void open();
-
-   /**
-    * @brief Close the file
-    */
-   void close();
-
-   /**
-    * @brief Parse the XML content
-    */
-   void parse();
-
-   /**
-    * @brief Check compatibility of opened file
-    */
-   void checkCompatibility();
-
-   /**
-    * @brief templated function to read value from XML tree
-    *
-    * rapidXML reads the value from the file as a string. The conversion to an
-    * other format is selected by the template parameter (ie. type of val). If
-    * wrong type is given the obtained value will be wrong.
-    *
-    * @param val  Storage for read value
-    * @param base XML tree node
-    * @param tag  XML tag to get value
-    *
-    * \tparam T Type of the value to read
-    */
-   template <typename T>
-   void readValue(T& val, rapidxml::xml_node<>* base, const std::string& tag);
-
-private:
-};
-
-template <typename T>
-void IXmlReader::readValue(T& val, rapidxml::xml_node<>* base,
-   const std::string& tag)
-{
-   // Create string stream to do type conversion
-   std::istringstream iss;
-   std::string tmp;
-
-   // Extract node for given tag
-   rapidxml::xml_node<>* vnode = base->first_node(tag.c_str());
-
-   // Check for successful node extraction
-   if (vnode)
+   class IXmlReader: public XmlFile
    {
-      // Get node value
-      tmp = vnode->value();
+      public:
+         /**
+         * @brief Constructor
+         *
+         * @param name     Filename
+         * @param ext      File extension
+         * @param header   Header string of file
+         * @param type     Type string of the file
+         * @param version  Version string of the file
+         * @param root     XML root string of the file
+         */
+         IXmlReader(std::string name, std::string ext, std::string header, std::string type, std::string version, std::string root);
 
-      // Convert node value string into template type
-      iss.str(tmp);
-      iss >> val;
-      iss.clear();
-   }
-   else
+         /**
+         * @brief Destructor
+         */
+         virtual ~IXmlReader();
+
+         /**
+          * @brief Initialise the file
+          */
+         virtual void init();
+
+         /**
+          * @brief Read the content
+          */
+         virtual void read() = 0;
+
+         /**
+          * @brief Finalise the file
+          */
+         virtual void finalize();
+
+      protected:
+         /**
+          * @brief Handle to the file
+          */
+         std::ifstream mFile;
+
+         /**
+          * @brief Content of file converted into a vector
+          */
+         std::vector<char> mContent;
+
+         /**
+          * @brief Open the file
+          */
+         void open();
+
+         /**
+          * @brief Close the file
+          */
+         void close();
+
+         /**
+          * @brief Parse the XML content
+          */
+         void parse();
+
+         /**
+          * @brief Check compatibility of opened file
+          */
+         void checkCompatibility();
+
+         /**
+          * @brief templated function to read value from XML tree
+          *
+          * rapidXML reads the value from the file as a string. The conversion to an other format is selected
+          * by the template parameter (ie. type of val). If wrong type is given the obtained value will be wrong.
+          *
+          * @param val  Storage for read value
+          * @param base XML tree node
+          * @param tag  XML tag to get value
+          *
+          * \tparam T Type of the value to read
+          */
+         template <typename T> void readValue(T& val, rapidxml::xml_node<>* base, const std::string& tag);
+
+      private:
+   };
+
+   template <typename T>  void IXmlReader::readValue(T& val, rapidxml::xml_node<>* base, const std::string& tag)
    {
-      throw std::logic_error("XML tag not found " + tag + "!");
+      // Create string stream to do type conversion
+      std::istringstream   iss;
+      std::string tmp;
+
+      // Extract node for given tag
+      rapidxml::xml_node<> *vnode = base->first_node(tag.c_str());
+
+      // Check for successful node extraction
+      if(vnode)
+      {
+         // Get node value
+         tmp = vnode->value();
+
+         // Convert node value string into template type
+         iss.str(tmp);
+         iss >> val;
+         iss.clear();
+
+      } else
+      {
+         throw std::logic_error("XML tag not found " + tag + "!");
+      }
    }
+
 }
-
-} // namespace Xml
-} // namespace Io
-} // namespace QuICC
+}
+}
 
 #endif // QUICC_IO_XML_IXMLREADER_HPP
