a13bf3d164c712291106f648b8196c28f6d81fb3
Giacomo Castiglioni <giacastiglioni@gmail.com>
2025-04-02 16:53:39 +0200

    add cache for slice size compute

--- Components/Graph/Graph/Shims/MlirAllocShims.cu
+++ Components/Graph/Graph/Shims/MlirAllocShims.cu
@@ -1,3 +1,5 @@
+#include <map>
+
 #include "Graph/Shims/MlirShims.hpp"
 
 namespace QuICC
@@ -7,6 +9,48 @@ namespace Graph
 
 namespace details
 {
+
+/// @brief Cache class
+/// Computing and copying from device to host is expensive.
+/// We cache the size of the layers in a map based on the metadata pointer.
+/// @tparam T type of ptr to cache
+template <class T>
+class Cache
+{
+public:
+    /// @brief delete copy ctor
+    /// @param
+    Cache(Cache const&) = delete;
+    /// @brief delete assignment op
+    /// @param
+    void operator=(Cache const&) = delete;
+    /// @brief create/return singleton
+    /// @return singleton
+    static Cache& getInstance();
+    /// @brief access the memory resource
+    /// @return
+    std::map<T*, std::size_t>& getMap();
+private:
+    /// @brief map pointe to size
+    std::map<T*, std::size_t> _ptr2size;
+    /// @brief Default ctor
+    Cache() = default;
+    /// @brief dtor
+    ~Cache() = default;
+};
+
+template <class T>
+Cache<T>& Cache<T>::getInstance()
+{
+    static Cache instance;
+    return instance;
+}
+
+template <class T>
+std::map<T*, std::size_t>& Cache<T>::getMap()
+{
+    return _ptr2size;
+}
 
 
 __global__ void kernelGetSizeS1CLCSC3DJIK(std::uint32_t* pCumSliceSize, const std::uint32_t* ptr, const std::uint32_t size, const std::uint32_t lds)
@@ -22,9 +66,22 @@ __global__ void kernelGetSizeS1CLCSC3DJIK(std::uint32_t* pCumSliceSize, const st
     *pCumSliceSize = cumSliceSize;
 }
 
-
+/// @brief get the cumulative size of the layers
+/// @param ptr pointe to ptr metadata
+/// @param size
+/// @param lds
+/// @return size
 std::uint32_t getSizeS1CLCSC3DJIK(const std::uint32_t* ptr, const std::uint32_t size, const std::uint32_t lds)
 {
+    // check if the size was cached
+    auto& cache = Cache<const std::uint32_t>::getInstance();
+    auto it = cache.getMap().find(ptr);
+    if (it != cache.getMap().end()) {
+        return it->second;
+    }
+
+    // otherwise, we need to calculate the size
+
     // setup grid
     dim3 blockSize;
     dim3 numBlocks;
@@ -42,6 +99,9 @@ std::uint32_t getSizeS1CLCSC3DJIK(const std::uint32_t* ptr, const std::uint32_t 
     kernelGetSizeS1CLCSC3DJIK<<<numBlocks, blockSize>>>(pCumSliceSize, ptr, size, lds);
     cudaErrChk(cudaMemcpy(&cumSliceSize, pCumSliceSize, sizeof(std::uint32_t), cudaMemcpyDeviceToHost));
     cudaErrChk(cudaFree(pCumSliceSize));
+
+    // cache the size
+    cache.getMap().insert(std::make_pair(ptr, cumSliceSize));
     return cumSliceSize;
 }
 
