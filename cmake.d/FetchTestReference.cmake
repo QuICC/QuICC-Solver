017646068b2a8021382b79866165452f758fe93b
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-04-02 10:17:49 +0200

    Fix data tag

--- cmake.d/FetchTestReference.cmake
+++ cmake.d/FetchTestReference.cmake
@@ -32,7 +32,7 @@ function(quicc_fetch_test_reference target)
     FILENAME "${QAT_FILENAME}"
     ARCHIVEDIR "${QAT_ARCHIVEDIR}"
     DATADIR "${QAT_DATADIR}"
-    GITTAG "chebyshev_tests"
+    GITTAG "v1.0.63"
     GITURL "https://gitlab.ethz.ch/quicc/test-testdata"
     )
 
