a2aefb317bb41f0699844cdf5005cc29ef0233b6
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-06-12 10:43:18 +0200

    Revert

--- Scripts/ParaView/PythonQuICCReader.py
+++ Scripts/ParaView/PythonQuICCReader.py
@@ -68,7 +68,7 @@ class PythonQuICCReaderBase(VTKPythonAlgorithmBase):
         self._scheme = self._current_file['/'].attrs['type']
 
         # Spherical schemes
-        if self._scheme in [b'SLFl', b'SLFm', b'WLFl', b'WLFm', b'JLFl', b'JLFm']:
+        if self._scheme in [b'SLFl', b'SLFm', b'WLFl', b'WLFm']:
             self._components = ['r', 'theta', 'phi']
             self._ijk_components = ['phi', 'theta', 'r']
             self._make_mesh_coordinates = self._spherical_mesh
