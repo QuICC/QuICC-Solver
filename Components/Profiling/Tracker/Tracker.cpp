fd6da16e77a1dae2431702603e7397758842795f
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-06-24 09:20:38 +0200

    Generate unique profiling hdf5

--- Components/Profiling/Tracker/Tracker.cpp
+++ Components/Profiling/Tracker/Tracker.cpp
@@ -15,6 +15,8 @@
 #include <highfive/H5DataSet.hpp>
 #include <highfive/H5DataSpace.hpp>
 #include <highfive/H5File.hpp>
+#include <random>
+#include <algorithm>
 #endif
 
 #include "PeakRss.hpp"
@@ -23,8 +25,24 @@
 #include <cuda_runtime_api.h>
 #endif
 
+#define QUICC_PROFILE_NATIVE_WRITER_HIGHFIVE_UNIQUEFILE
+
 namespace QuICC {
 namespace Profiler {
+
+namespace details {
+   std::string generate_random_string(size_t length)
+   {
+       const std::string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+       std::random_device random_device;
+       std::mt19937 generator(random_device());
+
+       std::string random_string(characters);
+       std::shuffle(random_string.begin(), random_string.end(), generator);
+
+       return random_string.substr(0, length);
+   }
+}
 
 
 #ifdef QUICC_MPI
@@ -123,7 +141,12 @@ void Tracker::print_hdf5()
 {
 #ifdef QUICC_PROFILE_NATIVE_WRITER_HIGHFIVE
 
+#ifdef QUICC_PROFILE_NATIVE_WRITER_HIGHFIVE_UNIQUEFILE
+    std::string fid = details::generate_random_string(6);
+    std::string outFile = "profile_" + fid + ".hdf5";
+#else
     std::string outFile = "profile.hdf5";
+#endif
 
     int rank{};
     int nRanks{1};
