5876e1a79d99e9c2476381c5f158734b096e6fb2
Philippe Marti (Avalon) <philippe.marti@colorado.edu>
2025-06-27 15:39:53 +0200

    1 socket test

--- ci/gitlab/bin/quicc/gitlab/pipelines.py
+++ ci/gitlab/bin/quicc/gitlab/pipelines.py
@@ -132,10 +132,7 @@ class libtest_pipeline(base_pipeline):
         self.config['test-quicc-lib'] = {
                 'variables':
                     {
-                        'SLURM_NTASKS': 1,
-                        'SLURM_NTASKS_PER_NODE': 1,
-                        'SLURM_CPUS_PER_TASK': str(self.cpus_full_node),
-                        'TEST_NCPU': str(self.cpus_full_node)
+                        'TEST_NCPU': str(self.cpus_full_socket)
                     },
                 'extends':
                     [
