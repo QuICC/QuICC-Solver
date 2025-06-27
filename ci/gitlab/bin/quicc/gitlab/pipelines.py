68bb8cfaa7f80df2d697f96e9d42fa32fd6c6588
Philippe Marti (Avalon) <philippe.marti@colorado.edu>
2025-06-27 14:29:23 +0200

    Use full node for CI

--- ci/gitlab/bin/quicc/gitlab/pipelines.py
+++ ci/gitlab/bin/quicc/gitlab/pipelines.py
@@ -20,6 +20,14 @@ backend2nodeSize = {
     "alps-zen2": 128
 }
 
+backend2socketSize = {
+    "daint-mc": 72,
+    "daint-gpu": 24,
+    "alps-a100": 64,
+    "alps-gh200": 72,
+    "alps-zen2": 64
+}
+
 """Base class, defines a pipeline that build the docker image, the library and cleans up the runner"""
 class base_pipeline(base_yaml):
     def __init__(self, cnf):
@@ -28,6 +36,7 @@ class base_pipeline(base_yaml):
         self.backend = cnf.backend
         self.base_docker = f'ci/docker/baseimage/Dockerfile_quicc_{cnf.image}_{self.backend}'
         self.cpus_full_node = backend2nodeSize[self.backend]
+        self.cpus_full_socket = backend2socketSize[self.backend]
         base_md5sum = hashlib.md5(open(self.base_docker[3:], 'rb').read()).hexdigest()
         self.tag = cnf.tag
         image = 'quicc_'+cnf.tag+'_'+cnf.backend+':$CI_COMMIT_SHA'
@@ -123,6 +132,9 @@ class libtest_pipeline(base_pipeline):
         self.config['test-quicc-lib'] = {
                 'variables':
                     {
+                        'SLURM_NTASKS': 1,
+                        'SLURM_NTASKS_PER_NODE': 1,
+                        'SLURM_CPUS_PER_TASK': str(self.cpus_full_node)
                         'TEST_NCPU': str(self.cpus_full_node)
                     },
                 'extends':
