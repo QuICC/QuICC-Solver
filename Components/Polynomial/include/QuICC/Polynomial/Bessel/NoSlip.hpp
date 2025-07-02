c0604058a19a4ddf545349bbc852a457a40ca787
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-07-02 17:51:22 +0200

    Fix typo

--- Components/Polynomial/include/QuICC/Polynomial/Bessel/NoSlip.hpp
+++ Components/Polynomial/include/QuICC/Polynomial/Bessel/NoSlip.hpp
@@ -67,7 +67,7 @@ inline void NoSlip<TOp>::compute(
    {
       std::vector<Internal::MHDFloat> roots_extra = {};
       SphericalBessel::getRoots(roots_extra, l + TOp::EXTRA_L, nPoly + TOp::EXTRA_POLY,
-         SphericalBessel::Value_dNu());
+         SphericalBessel::NoSlip_dNu());
 
       TOp::compute(rOut, roots, roots_extra, l, igrid, scale,
          SphericalBessel::NoSlip_dNu(), SphericalBessel::Value_dNu());
