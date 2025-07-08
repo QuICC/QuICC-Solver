d48d5a3a39d390a54323179e7488846b1e55bfcf
Philippe Marti (Avalon) <philippe.marti@colorado.edu>
2025-07-08 08:48:02 +0200

    More clenaup

--- Components/Framework/cmake.d/setup/RegisterTransformReductor.cmake
+++ Components/Framework/cmake.d/setup/RegisterTransformReductor.cmake
@@ -12,36 +12,6 @@ set(tags
   RadialPower
   RadialPowerOverr1
   RadialPowerOverr1D1R1
-  ValueEnergy
-  ValueEnergyD1
-  ValueEnergyD1R1
-  ValueEnergyR2
-  ValueEnergySlaplR2
-  ValuePower
-  ValuePowerD1
-  ValuePowerD1R1
-  ValuePowerR2
-  ValuePowerSlaplR2
-  InsulatingEnergy
-  InsulatingEnergyD1
-  InsulatingEnergyD1R1
-  InsulatingEnergyR2
-  InsulatingEnergySlaplR2
-  InsulatingPower
-  InsulatingPowerD1
-  InsulatingPowerD1R1
-  InsulatingPowerR2
-  InsulatingPowerSlaplR2
-  NoSlipEnergy
-  NoSlipEnergyD1
-  NoSlipEnergyD1R1
-  NoSlipEnergyR2
-  NoSlipEnergySlaplR2
-  NoSlipPower
-  NoSlipPowerD1
-  NoSlipPowerD1R1
-  NoSlipPowerR2
-  NoSlipPowerSlaplR2
   )
 
 include(RegisterTags)
