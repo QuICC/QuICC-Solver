5aa527f46c59b5e2645f4d52170703778c8d1634
Stefano Maffei <maffei.ste@gmail.com>
2025-09-26 23:44:43 +0200

    added test to ohmic dissipation

--- Components/Framework/include/QuICC/PhysicalOperators/SphericalOhmicDissipationAnelastic.hpp
+++ Components/Framework/include/QuICC/PhysicalOperators/SphericalOhmicDissipationAnelastic.hpp
@@ -39,11 +39,24 @@ namespace Physical {
    {
       public:
          /**
-          * @brief Add Coriolis term to S
+          * @brief Add Ohmic dissipation term to S
           */
          static void add(Framework::Selector::PhysicalScalarField &rS, 
                          const Resolution& res, 
                          const Array& r, 
+                         const Datatypes::VectorField<Framework::Selector::PhysicalScalarField, FieldComponents::Physical::Id> &w, 
+                         std::shared_ptr<QuICC::DenseSM::Chebyshev::LinearMap::RadialTorPolFunction> pF,
+                         const QuICC::Equations::EquationParameters &eqParams,
+                         const MHDFloat c = 1.0);
+         
+         /**
+          * @brief test Ohmic dissipation term to S
+          */
+         static void test(Framework::Selector::PhysicalScalarField &rS, 
+                         const Resolution& res, 
+                         const Array& r, 
+                         const Array& thGrid,    // Add theta grid
+                         const Array& phGrid,    // Add phi grid 
                          const Datatypes::VectorField<Framework::Selector::PhysicalScalarField, FieldComponents::Physical::Id> &w, 
                          std::shared_ptr<QuICC::DenseSM::Chebyshev::LinearMap::RadialTorPolFunction> pF,
                          const QuICC::Equations::EquationParameters &eqParams,
