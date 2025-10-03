575ee6ba8cb1f85b12a6c0e3fa411b318b779014
Stefano Maffei <maffei.ste@gmail.com>
2025-10-03 15:40:54 +0200

    corrected lorentz force

--- Components/Framework/include/QuICC/PhysicalOperators/SphericalLorentzAnelastic.hpp
+++ Components/Framework/include/QuICC/PhysicalOperators/SphericalLorentzAnelastic.hpp
@@ -65,6 +65,21 @@ namespace Physical {
                          const QuICC::Equations::EquationParameters &eqParams,
                          const MHDFloat c = 1.0);
 
+         /**
+          * @brief Add Lorentz term to S
+          */
+         static void test(Framework::Selector::PhysicalScalarField &rS, 
+                         FieldComponents::Physical::Id compId, 
+                         const Resolution& res, 
+                         const Array& r, 
+                         const Array& thGrid,    // Add theta grid
+                         const Array& phGrid,    // Add phi grid 
+                         const Datatypes::VectorField<Framework::Selector::PhysicalScalarField, FieldComponents::Physical::Id> &v, 
+                         const Datatypes::VectorField<Framework::Selector::PhysicalScalarField, FieldComponents::Physical::Id> &w, 
+                         std::shared_ptr<QuICC::DenseSM::Chebyshev::LinearMap::RadialTorPolFunction> pF,
+                         const QuICC::Equations::EquationParameters &eqParams,
+                         const MHDFloat c = 1.0);
+
       protected:
 
       private:
