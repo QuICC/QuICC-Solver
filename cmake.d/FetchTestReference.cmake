ff0b16e7eb355354fedc968ba6bbc65bc6aefe45
Stefano Maffei <maffei.ste@gmail.com>
2025-11-24 17:12:04 +0000

    updated cmake and sha for new tests

--- cmake.d/FetchTestReference.cmake
+++ cmake.d/FetchTestReference.cmake
@@ -32,7 +32,7 @@ function(quicc_fetch_test_reference target)
     FILENAME "${QAT_FILENAME}"
     ARCHIVEDIR "${QAT_ARCHIVEDIR}"
     DATADIR "${QAT_DATADIR}"
-    GITTAG "v1.0.69"
+    GITTAG "anelastic_tests"
     GITURL "https://gitlab.ethz.ch/quicc/test-testdata"
     )
 
