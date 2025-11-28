42e8a6229ff269333b2585c67a3df00f005b45a5
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-11-28 15:43:11 +0100

    Update gitlab tag

--- cmake.d/FetchTestReference.cmake
+++ cmake.d/FetchTestReference.cmake
@@ -32,7 +32,7 @@ function(quicc_fetch_test_reference target)
     FILENAME "${QAT_FILENAME}"
     ARCHIVEDIR "${QAT_ARCHIVEDIR}"
     DATADIR "${QAT_DATADIR}"
-    GITTAG "anelastic_tests"
+    GITTAG "v1.0.70"
     GITURL "https://gitlab.ethz.ch/quicc/test-testdata"
     )
 
